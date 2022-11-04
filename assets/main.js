c1ed18190df8e5acd1c5c30fcd96b1be2de51f85
William Huang <hh921088586@berkeley.edu>
2022-11-03 22:28:31 -0700

    finish html page and working on JS

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+let city = $('.city');
+let wind = $('.wind');
+let humidity = $(".humidity");
+let temp = $(".temp");
+
+let searchArr = [];
+let APIKey = "&appid=b9330bfd0b1bf5fe0c849c27df315565";
+
+$(document).ready(function() {
+    
+} )
